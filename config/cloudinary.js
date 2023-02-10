19040dfd196f4994068bb007efc438b1f09a4bc9
Hien Nguyen <nguyenthanhhienbmt@gmail.com>
2023-02-10 21:38:36 +0700

    feat(upload): upload file to cloudinary

---
+++ config/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "di21gcodw",
+  api_key: "789668554536589",
+  api_secret: "L_mzqwVJ1dAl5GcRPim8VWbb0UM",
+});
+
+module.exports = cloudinary;
---
+++ utils/index.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const cloudinary = require("../config/cloudinary");
+const DatauriParser = require("datauri/parser");
+const parser = new DatauriParser();
+const jwt = require("jsonwebtoken");
+
+exports.uploadToCloudinary = async (file) => {
+  try {
+    const extName = path.extname(file.originalname).toString();
+    const file64 = parser.format(extName, file.buffer);
+
+    const uploadedResponse = await cloudinary.uploader.upload(file64.content, {
+      upload_preset: "devto",
+    });
+    return uploadedResponse.url;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.createtoken = (id, email) => {
+  let token;
+  try {
+    token = jwt.sign({ userId: id, email: email }, "secret", {
+      expiresIn: "6h",
+    });
+  } catch (err) {
+    throw new Error("Fail to create token!");
+  }
+  return token;
+};
